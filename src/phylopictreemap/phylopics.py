b1d20e988830cbdeacb3f57c228dd2835af8c6f8
DAWells <david.arthur.wells@gmail.com>
2026-02-02 08:35:38 +0000

    Get phylopic cleanly based on name using open tree

--- src/phylopictreemap/phylopics.py
+++ src/phylopictreemap/phylopics.py
@@ -2,6 +2,8 @@
 """
 import requests
 from opentree import OT
+from io import BytesIO
+from lxml import etree
 
 API_BASE = "https://api.phylopic.org"
 HEADERS = {"Accept": "application/vnd.phylopic.v2+json"}
@@ -36,3 +38,16 @@ def get_phylopic_svg_url_from_ott_ids(ott_ids:list[int])->str:
     svg_url = image_response.json().get("_links").get("vectorFile").get("href")
     return svg_url
 
+def get_svg(name:str):
+    # Get open tree taxonomy id for name
+    ott_id = OT.get_ottid_from_name(name)
+    # Get lineage ids in case species not available
+    lineage_ott_ids = get_lineage_ott_ids(ott_id)
+    ott_ids = [str(ott_id)] + lineage_ott_ids
+    # Get most specific svg url
+    svgurl = get_phylopic_svg_url_from_ott_ids(ott_ids)
+    # Get svg from url
+    response = requests.get(svgurl)
+    response.raise_for_status()
+    tree = etree.parse(BytesIO(response.content))
+    return tree
